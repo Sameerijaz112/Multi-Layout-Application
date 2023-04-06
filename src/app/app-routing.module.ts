7669f9e2789931a549a20cd493f5e26202e869fe
Sameer <sameerejaz970@gmail.com>
2023-04-06 15:07:16 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 const routes: Routes = [
+  /* dashboard */
   {path:'', component: DashboardLayoutComponent ,  
 
 children:[
    {path:'dashboard',
-  redirectTo:'./dashboard',
+  redirectTo:'/dashboard',
   pathMatch:'full'
   },
    {path:'dashboard',
     loadChildren:() => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   }
+]},
+
+/* auth */
+{path:'',component:AuthLayoutComponent,
+children:[
+  {path:'',
+  redirectTo:'/auth',
+  pathMatch:'full'
+},
+{path:'auth',
+loadChildren:() =>import('./auth/auth.module').then(m => m.AuthModule)
+}
 ]}
 ];
 
